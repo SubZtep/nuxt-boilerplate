9e05bc692ab190981dce9a2d482881d2611eb049
SubZtep <subztep@gmail.com>
2019-08-21 05:11:56 +0100

    First test run

---
+++ README.md
@@ -0,0 +1,24 @@
+# nuxt-boilerplate
+
+_WIP!_
+
+Minimal Nuxt.js installation without Vue CLI.
+
+## Additions
+
+- Decorators
+- ESLint
+- Prettier
+- Pug
+- Scss
+- TypeScript
+
+## CLI Commands
+
+```bash
+# Run app in dev mode
+$ yarn dev
+
+# Lint code
+$ yarn lint
+```
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+h1(:class="{[$style.inv]: loaded}") Hello World
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "nuxt-property-decorator"
+
+@Component
+export default class IndexPage extends Vue {
+  loaded = false
+
+  mounted() {
+    this.loaded = true
+  }
+}
+</script>
+
+<style lang="scss" module>
+.inv {
+  background-color: black;
+  color: white;
+}
+</style>
